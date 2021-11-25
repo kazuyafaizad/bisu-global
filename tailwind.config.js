bfca4f385d5af61824ab95d35e1945037d3dd65a
kazuyafaizad <kazuyafaizad@gmail.com>
2021-11-25 16:46:09 +0800

    top page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  mode: "jit",
+  fontSize: false,
+  purge: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        golden: "#B8A273",
+        blacken: "#313131",
+        lighten: "#f4f3ef",
+      },
+    },
+  },
 };
